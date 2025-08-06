e56aa6b0f066f7d923b9a556b5787ab17dee5587
THENONT123_ <Nonthaphatc@hotmail.com_>
2025-08-06 15:26:19 +0700

    done

--- src/pages/HelloMyNamePage.tsx
+++ src/pages/HelloMyNamePage.tsx
@@ -1,4 +1,24 @@
+import { useState } from "react";
+
 export default function HelloMyNamePage() {
+  const [name,setName] = useState("");
+  const[result, setResult] = useState("Name Please");
+  const [isGreetOk,setGreetOk] = useState(false);
+
+  const nameInput = (event:any) => {
+    setName(event.target.value);
+  };
+  const addName = () => {
+      if(name === ""){
+        setResult("Name Please");
+        setGreetOk(false);
+      }
+      else {
+        setResult(`What's up ${name}`);
+        setGreetOk(true);
+      }
+  };
+
   return (
     <div className="container text-center">
       <h3>Hello My Name</h3>
@@ -6,10 +26,12 @@ export default function HelloMyNamePage() {
         className="form-control d-inline m-1"
         placeholder="Insert your name here!"
         style={{ width: "300px" }}
+        onChange={nameInput}
+        value = {name}
       />
-      <button className="btn btn-primary">Greet Me</button>
+      <button className="btn btn-primary" onClick = {addName}>Greet Me</button>
       {/* Result Text */}
-      <p></p>
+      <p className = {isGreetOk ? "" : "text-danger"}>{result}</p>
     </div>
   );
 }
